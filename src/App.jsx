1ce7355d165a41cf19ca05ccf226d5602cff32c2
Kenneth Rebello <kennethrebello253@gmail.com>
2024-09-26 15:24:55 +0530

    Added Button
    Marker, features and hero.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
 import Header from "./sections/Header.jsx";
+import Hero from "./sections/Hero.jsx";
+import { Features } from "./sections/Features.jsx";
 
 const App = () => {
-    return (
-        <main className="overflow-hidden">
-            <Header />
-        </main>
-    )
-}
-export default App
+  return (
+    <main className="overflow-hidden">
+      <Header />
+      <Hero />
+      <Features />
+    </main>
+  );
+};
+export default App;
